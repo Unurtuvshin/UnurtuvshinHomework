616d42fd210e7a13bcc0f7ea4d4dadd740aa80e0
Unurtuvshin <unur0625@gmail.com>
2022-08-26 22:55:29 +0800

    End of question 5, some notes

--- Homework1.py
+++ Homework1.py
@@ -11,3 +11,10 @@ print("Таны дурай уух зүйл бол {}, хоол бол {}.".forma
 song = str(input("Та дуртай дууны нэрийг бичнэ үү: "))
 print("Та {} дуунд дуртай.".format(song))
 #End of question 4
+bool1 = input("Хэрэв та багш бол 1, сурагч бол 0 гэж бичнэ үү: ")
+# bool(input()) гэж бичвэл хоосон биш string-ийг boolean-руу хөрвүүлсэн болох буюу 0 гэж өгсөн ч boolean нь True болдог юм байна. 
+if bool1==True:
+    print("Та багш байна.")
+else:
+    print("Та сурагч байна.")
+#End of question 5
