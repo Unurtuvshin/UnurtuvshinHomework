88fca5177d9511b0ec5de5e3c4dce79a873eb56b
Unurtuvshin <unur0625@gmail.com>
2022-08-26 22:36:13 +0800

    Edit question 1, End of question 2

--- Homework1.py
+++ Homework1.py
@@ -1,3 +1,6 @@
-name = string(input(("Нэрээ бичнэ үү: ")))
-print("Сайн байна уу? ", name)
+name = str(input("Нэрээ бичнэ үү: "))
+print("Сайн байна уу? ", name+ ".")
 # End of question 1
+nas = int(input("Насаа оруулна уу: "))
+print("Та {} настай.".format(nas))
+# End of question 2
