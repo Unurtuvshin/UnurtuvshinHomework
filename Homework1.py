b95e07012a66742e0636042a9d0f5c7712af6ffb
Unurtuvshin <unur0625@gmail.com>
2022-08-26 22:41:14 +0800

    End of question 3

--- Homework1.py
+++ Homework1.py
@@ -4,3 +4,7 @@ print("Сайн байна уу? ", name+ ".")
 nas = int(input("Насаа оруулна уу: "))
 print("Та {} настай.".format(nas))
 # End of question 2
+food = str(input("Та дуртай хоолоо бичнэ үү: "))
+drink = str(input("Та дуртай уух зүйлээ бичнэ үү: "))
+print("Таны дурай уух зүйл бол {}, хоол бол {}.".format(drink, food))
+#End of question 3
--- azifile.py
+++
@@ -1,4 +0,0 @@
-print("NiEmiSoEz")
-print("Whatisthat2")
-print("eee3")
-print("Wtfisgoingon4")
