6bee31407b63c8c11dd2f98db70704edf3f8dbd2
Unurtuvshin <unur0625@gmail.com>
2022-08-27 16:47:53 +0800

    Question 2, comment deer asuult uldeesen

--- Homework2/HomeWork2_Task1.py
+++ Homework2/HomeWork2_Task1.py
@@ -11,3 +11,9 @@ my_list.extend(my_list2) #my_list2-iig araas ni zalgah
 my_list.remove("nemelt") #nemelt gesen elementiig urdaas ni 1 udaa hasah
 my_list.pop(1) #2r element hasah, herev ()-dotor bairshil zaahgui bol suuliin elementiig hasna
 del my_list[1] #2r elemntiig hasah
+#Question 2
+import datetime #Yg yu import hiij baigaag asuuh
+udur_str = "2004-06-25" #String-dee utga ugsun
+udur_date = datetime.datetime.strptime(udur_str, "%Y-%m-%d") #String ee datetime huvisagch bolgoson, davhar datetime gej avj baigaag asuuh
+my_str = udur_date.strftime("%Y onii %m sariin %d udur tursun baina") #Datetime huvisagchaa string bolgoson
+print(my_str) #String-ee hevleh
